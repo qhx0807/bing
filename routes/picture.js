7ae52fcf6f9dcb7a3779fa45c0a6be97ce7dc891
qhx0807 <820240134@qq.com>
2018-04-24 22:37:05 +0800

    some update

--- routes/picture.js
+++ routes/picture.js
@@ -4,12 +4,22 @@ var router = express.Router()
 var bingUtil = require('../utlis/bingUtil')
 var dbUtil = require('../utlis/dbUtil')
 
-router.get('/bing/:idx', function(req, res, next) {
+router.get('/bing/:idx', function (req, res, next) {
   var opts = {}
   bingUtil.fetchDailyPic(opts, function (data) {
     dbUtil.insert('picture', data, function(err){
       res.json({d: err})
     })
+  })
+})
+
+router.get('/pic', function(req, res, next) {
+  var opts = {
+    query: '',
+    page: {page: 1, num: 10}
+  }
+  dbUtil.get('picture', opts, function (data) {
+    res.json(data)
   })
 })
 
