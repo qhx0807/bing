ed41c3e4ce3eba11c65a548a4db4f2b8c4be3841
qhx0807 <820240134@qq.com>
2018-04-24 22:36:56 +0800

    update dbutils

--- utlis/dbUtil.js
+++ utlis/dbUtil.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var dbConfig = require('../config/db')
+var objectAssign = require('object-assign')
 
 var pool = mysql.createPool(dbConfig.bing)
 
@@ -33,7 +34,7 @@ module.exports = {
    * 查询
    *
    * @param {string} table
-   * @param {any} params
+   * @param {any} params {page:0, num: 1}
    * @param {func} callback
    */
   get: function (table, params, callback) {
@@ -41,7 +42,45 @@ module.exports = {
       page: 1,
       num: 10
     }
+    var condition = []
+    var query = params.query || {}
+    var page = params.page || {}
+    defaultPage = objectAssign(defaultPage, page)
+    var sql = 'select * from ' + table
+    if (Object.prototype.toString.call(query) === '[object Object]') {
+      for (var i in query) {
+        condition.push(i + '="' + query[i] + '"')
+      }
+      if (condition.length > 0) {
+        sql += ' where ' + condition.join(' and ')
+      }
+    } else if (Object.prototype.toString.call(query) === '[object String]') {
+        sql += ' where ' + query;
+    }
+    sql += ' order by id desc limit ' + (defaultPage.page - 1) * defaultPage.num + ',' + defaultPage.num
+    module.exports.sqlQuery(sql, callback)
+  },
 
+  /**
+   * 获得总条数
+   * @table   表名
+   * @params  参数{k:v}
+   * @callback
+   */
+  getCount: function(table, params, callback) {
+    var sql = 'select count(id) as sum from ' + table
+    var _condition = [];
+    if (Object.prototype.toString.call(params) === '[object Object]') {
+        for (var i in params) {
+            _condition.push(i + '="' + params[i] + '"')
+        }
+        if (_condition.length > 0) {
+            sql += ' where ' + _condition.join(' and ')
+        }
+    } else if (Object.prototype.toString.call(params) === '[objcet String]') {
+        sql += ' where ' + params
+    }
+    module.exports.commonQuery(sql, callback)
   },
 
   /**
@@ -60,6 +99,30 @@ module.exports = {
     }
     var sql = 'insert into ' + table + '(' + keys.join(',') + ') values("' + vals.join('","') + '")'
     module.exports.sqlQuery(sql, callback)
+  },
+
+  /**
+   *
+   *
+   * @param {String} table
+   * @param {any} params
+   * @param {func} callback
+   */
+  update: function(table, params, callback) {
+    var body = params.body
+    var condition = params.condition
+    var body_temp = []
+    var condition_temp = []
+    for (var i in body) {
+        body_temp.push(i + '="' + body[i] + '"')
+    }
+    for (var j in condition) {
+        condition_temp.push(j + '="' + condition[j] + '"')
+    }
+    if (body_temp.length > 0 && condition_temp.length > 0) {
+        var sql = 'update ' + table + ' set ' + body_temp.join(',') + ' where ' + condition_temp.join(' and ')
+        module.exports.sqlQuery(sql, callback)
+    }
   }
 
 
