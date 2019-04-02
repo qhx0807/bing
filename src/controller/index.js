99cc4b86741783daa32a72de4859cff1708876cc
qhx0807 <820240134@qq.com>
2019-04-02 13:37:24 +0800

    index view

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,6 +2,18 @@ const Base = require('./base.js')
 
 module.exports = class extends Base {
   async indexAction () {
+    const page = this.get('p') || 1
+    const bingWallpaperModel = think.model('bing_wallpaper')
+    const data = await bingWallpaperModel.page(page).countSelect()
+    const obj = {
+      data: data.data,
+      currentPage: data.currentPage,
+      totalPages: data.totalPages,
+      pageSize: data.pageSize,
+      Prev: data.currentPage === 1 ? 1 : data.currentPage - 1,
+      Next: data.currentPage === data.totalPages ? data.totalPages : data.currentPage + 1
+    }
+    this.assign(obj)
     return this.display()
   }
 }
